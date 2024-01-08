216d60978f3d1517f2ae2b4c71e3889d1e6d0d63
YourUserNameOnGithub <kashish@mentorskool.com>
2024-01-08 17:45:00 +0530

    grouping test with greet

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,14 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import { Greet } from "./greet";
 import "@testing-library/jest-dom";
-test.skip("Greet render correctly", () => {
-  render(<Greet />);
-  const textElement = screen.getByText("Yo");
-  expect(textElement).toBeInTheDocument();
-});
 
-test.only("Greet render with a name", () => {
-  render(<Greet name="John" />);
-  const textElement = screen.getByText("Yo John");
-  expect(textElement).toBeInTheDocument();
+describe("Greet", () => {
+  test("render correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText("Yo");
+    expect(textElement).toBeInTheDocument();
+  });
+
+  test("render with a name", () => {
+    render(<Greet name="John" />);
+    const textElement = screen.getByText("Yo John");
+    expect(textElement).toBeInTheDocument();
+  });
 });
