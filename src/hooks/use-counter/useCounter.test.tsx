f08abb44f228fd4b7e410418ca9326ea44d661ee
YourUserNameOnGithub <kashish@mentorskool.com>
2024-01-15 23:25:06 +0530

    progress saved

--- src/hooks/use-counter/useCounter.test.tsx
+++ src/hooks/use-counter/useCounter.test.tsx
@@ -1,9 +1,26 @@
-import { renderHook } from "@testing-library/react";
+import { renderHook, act } from "@testing-library/react";
 import { useCounter } from "./useCounter";
 
 describe("useCounter", () => {
   test("should render initial count value", () => {
     const { result } = renderHook(useCounter);
     expect(result.current.count).toBe(0);
+  });
+
+  test("should accept and render the same initial count value", () => {
+    const { result } = renderHook(useCounter, {
+      initialProps: { initialCount: 10 },
+    });
+    expect(result.current.count).toBe(10);
+  });
+  test("should render the value after increment", () => {
+    const { result } = renderHook(useCounter);
+    act(() => result.current.increment());
+    expect(result.current.count).toBe(1);
+  });
+  test("should render the value after decrement", () => {
+    const { result } = renderHook(useCounter);
+    act(() => result.current.decrement());
+    expect(result.current.count).toBe(-1);
   });
 });
